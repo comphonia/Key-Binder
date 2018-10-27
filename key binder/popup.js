fbc4bcea030098732c5d82d2149e4d4f40dc6f10
comphonia <online.comphonia@gmail.com>
2018-10-27 01:43:32 -0500

    UI CRUD and functionality
    all ui displays correctly.
    failed testing:
    -  CSP => [Refused to execute inline event handler]
    - UI functionality not available outside its window
    - window doesn't stay outside its window

--- key binder/popup.js
+++ key binder/popup.js
@@ -1,3 +1,7 @@
+    $(function() {
+        Start();
+    });
+
 function Start() {
     $('.addBtn').click(function () {
         EnterBindMode();
@@ -6,21 +10,49 @@ function Start() {
 }
 
 var inBindMode = false;
+var tempKey=[];
+var storedKeys = []; // store[key] json name,id,class
 
-var bindElement = '<div class="col-auto mb-2 binder"> \
-            <input type="text" class="form-control mr-3" \ id="" placeholder="Key name" > \
-            </div> \
-            <span class="kbtn bg-success binder" onClick=AddKey()> <i class="fa fa-check"></i></span> <span class="currElement binder"></span>';
+//Add form
+var bindElement = '<div class="row binderBlock">\
+                <div class="col-auto mb-2 binder">\
+                    <input type="text" class="form-control mr-3" id="binderId" placeholder="Key name">\
+                </div>\
+                <span class="kbtn bg-success binder" onclick="AddKey()"> <i class="fa fa-check"></i></span> <span class="currElement binder"></span>\
+            </div>';
+//Key Form
+var keyElement = '<div class="row keyBlock">\
+                <div class="col-auto mb-2">\
+                    <input type="text" class="form-control mr-3" id="" placeholder="Key name" disabled>\
+                </div>\
+                <span class="kbtn bg-danger" onclick="RemoveKey()"> <i class="fa fa-times"></i></span>\
+            </div>';
+
 
 function AddElement() {
     $('#item-list').append(bindElement);
 }
 
+function AddKey() {
+    $('.binderBlock').before(keyElement);
+    storedKeys.push({name:"{name}",class:tempKey[0].class,id: tempKey[0].id});
+    tempKey.pop();
+    console.log(storedKeys);
+    
+    
+   
+}
+
+function RemoveKey()
+{
+    $('keyBlock').remove();   
+}
+
 function EnterBindMode() {
     if (!inBindMode) {
+        inBindMode = true;
         console.log('bind');
         AddElement();
-        inBindMode = true;
         $('html').addClass('crosshair');
         $('.addBtn').addClass('bg-danger');
         GetClickedElement();
@@ -31,15 +63,28 @@ function ExitBindMode() {
     if (inBindMode) {
         inBindMode = false;
         console.log('exit bind');
+        $('.noclick').css('pointer-events','nonw');
         $('html').removeClass('crosshair');
         $('.addBtn').removeClass('bg-danger');
-        $('.binder').detach();
+        $('.binderBlock').detach();
     }
 }
 
 function GetClickedElement() {
+    let counter =0;
     $("body").click(function (event) {
-        console.log(event.target.classList);
-        $(".currElement").html("clicked: " + event.target.classList);
+        if (counter === 0) {
+            counter++;
+        } else if (counter === 1) {
+            counter++;
+            $(".currElement").html("clicked: " + event.target.classList);
+            console.log(event.target.classList);
+            console.log(event.target.getAttribute('id'));
+            tempKey.push({class: event.target.classList[0],id: event.target.getAttribute('id')});
+            $('html').removeClass('crosshair');
+           $('.noclick').css('pointer-events','all');
+            console.log(counter);
+        }
+
     });
 }
